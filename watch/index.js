8e94acf43266556237a3d0bba8c7fefba7729b7a
Walter Dal Mut <walter.dalmut@gmail.com>
2017-09-23 16:38:40 +0200

    Fixes watch on single file

--- watch/index.js
+++ watch/index.js
@@ -4,13 +4,19 @@ var path = require('path');
 module.exports = function(events) {
   return function(basepath) {
     return fs.watch(basepath, {recursive: false}, (eventType, filename) => {
-      let fullpath = path.join(basepath, filename);
+      fs.stat(basepath, (err, stat) => {
+        let fullpath = basepath;
 
-      if (fs.existsSync(fullpath)) {
+        if (err) {
+          return events.emit('watch.remove', fullpath);
+        }
+
+        if (stat.isDirectory()) {
+          fullpath = path.join(basepath, filename);
+        }
+
         events.emit('watch.change', fullpath);
-      } else {
-        events.emit('watch.remove', fullpath);
-      }
+      });
     });
   };
 };
